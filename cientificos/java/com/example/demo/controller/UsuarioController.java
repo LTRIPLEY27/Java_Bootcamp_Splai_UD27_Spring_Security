67994b44caf934cc725758d844806ac7bc3b15c9
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-05 18:56:20 +0100

    PRUEBA

---
+++ cientificos/java/com/example/demo/controller/UsuarioController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  // LOS DECORADORES PARA EL TOKEN DE JSON DIFIEREN, EN UN MISMO SE ESPECÍFICAN LOS MÉTODOS CRUD A RESOLVER (GET, POST, PUT Y DELETE)
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.DELETE,RequestMethod.POST, RequestMethod.PUT})
+public class UsuarioController {
+
+	private IUsuarioDAO iUsuarioDAO;
+
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public UsuarioController(IUsuarioDAO iUsuarioDAO, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.iUsuarioDAO = iUsuarioDAO;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+	
+	
+	@GetMapping("/response-entity-builder-with-http-headers")
+	public ResponseEntity<String> usingResponseEntityBuilderAndHttpHeaders() {
+	    HttpHeaders responseHeaders = new HttpHeaders();
+	    responseHeaders.set("Baeldung-Example-Header", 
+	      "Value-ResponseEntityBuilderWithHttpHeaders");
+
+	    return ResponseEntity.ok()
+	      .headers(responseHeaders)
+	      .body("Response with header using ResponseEntity");
+	}
+	
+	@PostMapping("/users/")
+	public Usuario saveUsuario(@RequestBody Usuario user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		iUsuarioDAO.save(user);
+		return user;
+	}
+
+	@GetMapping("/users/")
+	public List<Usuario> getAllUsuarios() {
+		return iUsuarioDAO.findAll();
+	}
+
+	@GetMapping("/users/{username}")
+	public Usuario getUsuario(@PathVariable String username) {
+		return iUsuarioDAO.findByUsername(username);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public String eliminarUser(@PathVariable(name="id")long id) {
+		iUsuarioDAO.deleteById(id);
+		return "User deleted.";
+	}
+}
